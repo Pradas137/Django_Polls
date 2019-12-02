24942cae8a7ef8442a8e9f456c8fb418e737c8d2
Pradas137 <pradas137@gmail.com>
2019-12-02 16:44:44 +0100

    first commit

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,20 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
+
+class QuestionModelTests(TestCase):
+def  test_was_published_recently_with_old_question ( self ): 
+
+    time  =  timezone . ahora ()  -  fecha y hora . timedelta ( días = 1 ,  segundos = 1 ) 
+    old_question  =  Question ( pub_date = time ) 
+    self . afirmarIs ( antigua_pregunta . fue_publicada_recientemente(),  Falso ) 
+
+def  test_was_published_recently_with_recent_question ( self ): 
+   
+    Time  =  timezone . ahora ()  -  fecha y hora . timedelta ( horas = 23 ,  minutos = 59 ,  segundos = 59 ) 
+    Recent_question  =  Question ( pub_date = time ) 
+    self. afirmarIs ( reciente_pregunta . was_published_recently (),  True )
--- polls/views.py
+++ polls/views.py
@@ -2,19 +2,17 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
-
+from django.utils import timezone
 from .models import Choice, Question
 
+class  IndexView ( generic.ListView ): 
+    template_name  =  'polls/index.html' 
+    context_object_name  =  'latest_question_list' 
 
-class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
-    context_object_name = 'latest_question_list'
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
-
-
+    def  get_queryset ( self ): 
+        """Devuelve las últimas cinco preguntas publicadas.""" 
+        return Question.objects.order_by( '-pub_date' ) [: 5 ]
+        
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
